59ca6fab5ab922ce8d45e431aa9184f3ce12d258
sirius2206 <sergei_galkin@inbox.ru>
2021-11-06 14:50:02 +0500

    Домашнее задание №1

--- src/main/java/Lesson1/Participant.java
+++
@@ -1,9 +0,0 @@
-package Lesson1;
-
-public interface Participant {
-    public String getName();
-    public boolean isSuccess();
-    public void running();
-    public void jumping();
-    public void overcome(Obstacle obstacle);
-}
